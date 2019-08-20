285ca3d34e6267733643c8d3d9d3107f50941ca3
zien-zhang <zhangzien9527@163.com>
2019-08-20 19:44:58 +0800

    hello修改

--- gitTest/gitTest/src/cn/itcast/demo/HelloTest.java
+++ gitTest/gitTest/src/cn/itcast/demo/HelloTest.java
@@ -3,5 +3,6 @@ package cn.itcast.demo;
 public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
+        System.out.println("李四修改了");
     }
 }
