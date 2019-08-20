daee37807509ce72971c95512e775b9295999c4f
zien-zhang <zhangzien9527@163.com>
2019-08-20 20:23:32 +0800

    我是李四

--- gitTest/gitTest/src/cn/itcast/demo/HelloTest.java
+++ gitTest/gitTest/src/cn/itcast/demo/HelloTest.java
@@ -4,5 +4,6 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
         System.out.println("李四修改了");
+        System.out.println("aaaaaaaaaaaaaa");
     }
 }
